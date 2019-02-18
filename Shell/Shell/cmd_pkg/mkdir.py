dd3354f020bd883656a8235306ae1eca1c3af7b0
SAI RAMA KRISHNA TUMMALA <34177350+sairamktummala@users.noreply.github.com>
2019-02-17 21:16:27 -0600

    Delete mkdir.py

--- Shell/Shell/cmd_pkg/mkdir.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-def makedir(command):
-    file=command[1]
-    if not os.path.exists(file):
-        os.makedirs(file)
-    else:
-        sys.stdout.write('Directory already exists')
