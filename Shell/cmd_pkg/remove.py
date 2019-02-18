eca1ed5b66f827a376898f1d8a32c1fe90418912
SAI RAMA KRISHNA TUMMALA <34177350+sairamktummala@users.noreply.github.com>
2019-02-17 21:31:02 -0600

    Delete remove.py

--- Shell/cmd_pkg/remove.py
+++
@@ -1,8 +0,0 @@
-import os 
-import sys
-def remove(file):
-    if os.path.exists(file):
-         os.remove(file)
-    else:
-        sys.stdout.write('file not found')
-        sys.stdout.write('\n')
