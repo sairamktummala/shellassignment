0adb9915895383a02079e893c9210cec9257894f
SAI RAMA KRISHNA TUMMALA <34177350+sairamktummala@users.noreply.github.com>
2019-02-20 13:16:09 -0600

    Delete chmod.py

--- Shell/shell/cmd_pkg/chmod.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-def chmodfun(cmd):
-    filename = cmd[2]
-    str=int(cmd[1])
-    if os.path.isfile(filename):
-        os.chmod(filename,str)
-    else:
-        sys.stdout.write('file not found')
